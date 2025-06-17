86946d185731bcf34faa31525e7a7ecccc2e79db
Paulo Victor <paulovdbarbosa@gmail.com>
2025-06-17 10:24:02 -0300

    Sistema simples de gerenciamento de rotas com nodejs

---
+++ aula006/server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 3000;
+const host = 'localhost';
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+
+    let view = '';
+    res.statusCode = 200;
+    switch (req.url) {
+        case '/':
+            view = 'home';
+            break;
+        case '/services':
+            view = 'services';
+            break;
+        case '/about':
+            view = 'about';
+            break;
+        default:
+            view = '404';
+            res.statusCode = 404;
+            break;
+    }
+
+    const filePath = path.join(__dirname, 'pages', view + '.html');
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.log('Erro ao ler o arquivo:', err);
+            res.statusCode = 500;
+            res.end('<h1>Erro interno no servidor</h1>');
+        } else {
+            res.end(data);
+        }
+    });
+});
+
+server.listen(port, host, () => {
+    console.log(`Servidor iniciado em http://${host}:${port}`);
+});
