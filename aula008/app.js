3e6dddd1542abf09b7c7dc05f0aeb0768bdfae52
Paulo Victor <paulovdbarbosa@gmail.com>
2025-06-20 09:57:53 -0300

    Introdução as rotas do express

--- aula008/app.js
+++ aula008/app.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const app = express();
 
+const body = {
+    status: "success",
+    code: 200,
+    data: null
+};
+
+app.get('/home', (req, res) => {
+    body.data = "home";
+    res.send(
+        body
+    );
+})
+
 app.get('/', (req, res) => {
-    res.send('Olá ExpressJS!');
+    res.redirect('/home');
+})
+
+app.get('/about', (req, res) => {
+    body.data = "about";
+    res.send(body);
+})
+
+app.use((req, res) => {
+    body.status = "erro";
+    body.code = 404;
+    body.data = "rota não encontrada.";
+    res.status(404).send(body);
 })
 
 app.listen(3000);
